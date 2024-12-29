6735e855b78d1ada387a06ce7297a3f604e3b025
rowan2104 <36969990+rowan2104@users.noreply.github.com>
2024-12-29 11:35:42 +0000

    Fixed missing shader files

--- city.cpp
+++ city.cpp
@@ -3,7 +3,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include <render/shader.h>
+#include <shader.h>
 #include "animationData.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
